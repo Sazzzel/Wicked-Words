4151d0f7d04955ae97663acfc5bdd8ac6b31e928
Matthew Dixon <m.dixon5@outlook.com>
2024-10-22 15:20:42 +0000

    Add : function to select random word and scramble depending on game type

--- assets/script/words.js
+++ assets/script/words.js
@@ -1,39 +1,139 @@
 const words = [
-    // Difficulty 1 (short words: 3 letter)
-    ["bat", "cat", "boo", "web", "fog", "owl", "hat", "bat", "rat", "eye", 
-    "pot", "hex", "pit", "boo", "ash", "zap", "wig", "nun", "mud", "paw"],
+  // Difficulty 1 (short words: 3 letter)
+  [
+    "bat",
+    "cat",
+    "boo",
+    "web",
+    "fog",
+    "owl",
+    "hat",
+    "bat",
+    "rat",
+    "eye",
+    "pot",
+    "hex",
+    "pit",
+    "boo",
+    "ash",
+    "zap",
+    "wig",
+    "nun",
+    "mud",
+    "paw",
+  ],
 
-    //Difficulty 2 (4-letter words)
-    ["Ghost", "Tomb", "Witch", "Wolf", "Fear", "Moth", "Bats", "Dusk",
-     "Gory", "Cree", "Grim", "Wail", "Haze", "Lurk", "Dark", "Rave", 
-     "Spook", "Dead", "Skull", "Fangs"]
-    
+  //Difficulty 2 (4-letter words)
+  [
+    "tomb",
+    "wolf",
+    "fear",
+    "moth",
+    "bats",
+    "dusk",
+    "gory",
+    "cree",
+    "grim",
+    "wail",
+    "haze",
+    "lurk",
+    "dark",
+    "rave",
+    "dead",
+    "fang",
+  ],
+
+  [
     // Difficulty 3 (5-letter words)
-    ["witch", "ghost", "candy", "grave", "skull", "beast", "broom", "sneak", 
-    "fangs", "clown", "crypt", "siren", "crows", "moons", "potion", "treat", 
-    "ghoul", "spook", "bones", "torch"],
-    
-     // Difficulty 4 (6-letter words)
-    ["zombie", "haunts", "spider", "tombstone", "spooky", "goblin", "wizard", 
-    "cobweb", "pumpkin", "cauldron", "mystery", "dagger", "jackal", "spells", 
-    "devils", "candles", "scarer", "witches", "phantom", "demons"],
-    
-    // Difficulty 5 (7-letter words)
-    ["monster", "vampire", "grimace", "skeleton", "haunted", "darkness", 
-    "pumpkin", "creeper", "lurking", "gargoyle", "mummies", "phantom", 
-    "cauldron", "villain", "warlock", "nightmare", "screams", "shadows", 
-    "dungeon", "ghastly"],
-    
-    // Difficulty 6 (8-letter words)
-    ["frankenstein", "werewolf", "cauldron", "scarecrow", "graveyard", 
-    "phantoms", "demonic", "witchcraft", "skeletons", "nightfall", 
-    "darkness", "possession", "bloodlust", "mysterious", "paranormal", 
-    "sorcerer", "sacrificial", "undead", "grimreaper", "execution"],
-    
-    // Difficulty 7 (9-letter words)
-     ["possession", "supernatural", "witchcraft", "exorcism", "nightmares", 
-     "frightful", "mysterious", "necromancy", "werewolves", "apparition", 
-    "poltergeist", "vampirism", "lycanthrope", "necropolis", "bloodsucker", 
-    "macabre", "reanimator", "executioner", "cloaked", "bloodcurdling"],
-    
-]
+    "witch",
+    "ghost",
+    "candy",
+    "grave",
+    "skull",
+    "beast",
+    "broom",
+    "sneak",
+    "fangs",
+    "clown",
+    "crypt",
+    "siren",
+    "crows",
+    "moons",
+    "treat",
+    "ghoul",
+    "spook",
+    "bones",
+    "torch",
+  ],
+
+  // Difficulty 4 (6-letter words)
+  [
+    "zombie",
+    "haunts",
+    "spider",
+    "spooky",
+    "goblin",
+    "wizard",
+    "cobweb",
+    "dagger",
+    "jackal",
+    "spells",
+    "devils",
+    "candles",
+    "scarer",
+    "demons",
+    "potion",
+  ],
+
+  // Difficulty 5 (7-letter words)
+  [
+    "monster",
+    "vampire",
+    "grimace",
+    "skeleton",
+    "haunted",
+    "pumpkin",
+    "creeper",
+    "lurking",
+    "mummies",
+    "phantom",
+    "villain",
+    "warlock",
+    "screams",
+    "shadows",
+    "dungeon",
+    "ghastly",
+    "witches",
+    "mystery",
+  ],
+
+  // Difficulty 6 (8-letter words)
+  [
+    "werewolf",
+    "cauldron",
+    "phantoms",
+    "skeleton",
+    "darkness",
+    "sorcerer",
+    "exorcism",
+    "cauldron",
+    "gargoyle",
+    "darkness",
+    "cauldron",
+    "banshees",
+    "chilling",
+  ],
+];
+
+let level = 0;
+let puzzle = null;
+
+function newWord(){
+    puzzle= words[Math.floor(level - 1)][Math.floor(Math.random() * words.length)];
+      let shuffle = puzzle;
+    if(gameType === 'scramble'){
+       shuffle =  puzzle.split('').sort(function(){return 0.5-Math.random()}).join('');
+    }
+
+  document.getElementById("Puzzle").innerHTML = shuffle;
+}
