1528502259cbcaef3c4fa54a69a13c1b1ede63c4
Sarah Darlington <sazzzel@gmail.com>
2024-10-22 15:53:58 +0000

    Add: game.js file

---
+++ assets/script/game.js
@@ -0,0 +1,14 @@
+let canvas = document.getElementById('lests');
+    let ctx = canvas.getContext('2d');
+    let width = canvas.width;
+    let height = canvas.height;
+    let bats = [];
+    let batCount = 10;
+    let cast = true;
+    let ballSize = 0;
+
+    // draw wizard
+    function drawWizard() {
+        ctx.drawImage()
+        
+    }
