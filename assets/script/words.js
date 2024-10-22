b51f16f9223370810f775c2a71152861a5202438
Matthew Dixon <m.dixon5@outlook.com>
2024-10-22 14:15:46 +0000

    Add : word arrays to words.js file

---
+++ assets/script/words.js
@@ -0,0 +1,39 @@
+const words = [
+    // Difficulty 1 (short words: 3 letter)
+    ["bat", "cat", "boo", "web", "fog", "owl", "hat", "bat", "rat", "eye", 
+    "pot", "hex", "pit", "boo", "ash", "zap", "wig", "nun", "mud", "paw"],
+
+    //Difficulty 2 (4-letter words)
+    ["Ghost", "Tomb", "Witch", "Wolf", "Fear", "Moth", "Bats", "Dusk",
+     "Gory", "Cree", "Grim", "Wail", "Haze", "Lurk", "Dark", "Rave", 
+     "Spook", "Dead", "Skull", "Fangs"]
+    
+    // Difficulty 3 (5-letter words)
+    ["witch", "ghost", "candy", "grave", "skull", "beast", "broom", "sneak", 
+    "fangs", "clown", "crypt", "siren", "crows", "moons", "potion", "treat", 
+    "ghoul", "spook", "bones", "torch"],
+    
+     // Difficulty 4 (6-letter words)
+    ["zombie", "haunts", "spider", "tombstone", "spooky", "goblin", "wizard", 
+    "cobweb", "pumpkin", "cauldron", "mystery", "dagger", "jackal", "spells", 
+    "devils", "candles", "scarer", "witches", "phantom", "demons"],
+    
+    // Difficulty 5 (7-letter words)
+    ["monster", "vampire", "grimace", "skeleton", "haunted", "darkness", 
+    "pumpkin", "creeper", "lurking", "gargoyle", "mummies", "phantom", 
+    "cauldron", "villain", "warlock", "nightmare", "screams", "shadows", 
+    "dungeon", "ghastly"],
+    
+    // Difficulty 6 (8-letter words)
+    ["frankenstein", "werewolf", "cauldron", "scarecrow", "graveyard", 
+    "phantoms", "demonic", "witchcraft", "skeletons", "nightfall", 
+    "darkness", "possession", "bloodlust", "mysterious", "paranormal", 
+    "sorcerer", "sacrificial", "undead", "grimreaper", "execution"],
+    
+    // Difficulty 7 (9-letter words)
+     ["possession", "supernatural", "witchcraft", "exorcism", "nightmares", 
+     "frightful", "mysterious", "necromancy", "werewolves", "apparition", 
+    "poltergeist", "vampirism", "lycanthrope", "necropolis", "bloodsucker", 
+    "macabre", "reanimator", "executioner", "cloaked", "bloodcurdling"],
+    
+]
